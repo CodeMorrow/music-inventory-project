6d68f1f0385568f0323c5cef4237782969fbc33e
Alexander Noto <noto.alex.j@gmail.com>
2016-07-13 11:22:53 -0500

    add page

--- app/controllers/add_forms.rb
+++ app/controllers/add_forms.rb
@@ -2,14 +2,14 @@ MyApp.get "/add" do
 	erb :"add_forms/add"
 end
 
-MyApp.get "/add/album" do
+MyApp.get "/add_album" do
 	erb :"add_forms/add_album"	
 end
 
-MyApp.get "/add/artist" do
+MyApp.get "/add_artist" do
 	erb :"add_forms/add_artist"
 end
 
-MyApp.get "/add/song" do
+MyApp.get "/add_song" do
 	erb :"add_forms/add_song"
 end
