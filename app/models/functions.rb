34c1dc0e38bc3e9180cd75e5c1659793df2cd63d
Alexander Noto <noto.alex.j@gmail.com>
2016-07-14 09:37:04 -0500

    write to file successful

--- app/models/functions.rb
+++ app/models/functions.rb
@@ -1,35 +1,3 @@
-class Song
-  def initialize(title,artist,album,genre,minute,second,songrating,albumrating)
-    @title = title
-    @artist = artist
-    @album = album
-    @genre = genre
-    @lengthmin = minute
-    @lengthsec = second
-    @songrating = songrating
-    @albumrating = albumrating
-  end
-
-  def songinfo
-    return "#{@title}||#{@artist}||#{@album}||#{@genre}||#{@lengthmin}||#{@lengthsec}||#{@songrating}||#{@albumrating}"
-  end
-end
-
-
-
-song = Song.new("title","artist","album","genre","lengthmin", "lengthsec", "songrating", "albumrating")
-songfile = song.songinfo
-
-
-
-# Function adds the string being passed through the songfile variable into the 
-def songAdd2File(songfile)
-  File.open('music_db.txt', 'a') {|z| z.puts "#{songfile}" }
-end
-
-
-
-
 #Class that takes query string params. songinfo will return a string of song information, delineated by pipes
 #that can be written to a file.
 
@@ -46,7 +14,7 @@ class Song
   end
 
   def songinfo
-    return "#{@title}||#{@artist}||#{@album}||#{@genre}||#{@lengthmin}||#{@lengthsec}||#{@songrating}||#{@albumrating}"
+      return "#{@title}||#{@artist}||#{@album}||#{@genre}||#{@lengthmin}||#{@lengthsec}||#{@songrating}||#{@albumrating}"
   end
 end
 
@@ -71,11 +39,10 @@ end
 
 # If file exists, do nothing. Otherwise, create the music_db file with the first row being headers.
 def createFileWithHeader 
-  if File.exist?("music_db.txt")
-  else
-  open('music_db.txt', 'w') { |z|
-  z << "Artist||Album Title||Genre||Release Year||Song||Length||Song Rating||Album Rating\n"
-  }
+  if !File.exist?("music_db.txt")
+    open('music_db.txt', 'w') { |z|
+      z << "Artist||Album Title||Genre||Release Year||Song||Length||Song Rating||Album Rating\n"
+    }
   end 
 end
 
