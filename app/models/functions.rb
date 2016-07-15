aed9784644afc5b1436657d64ef0753d29a59d36
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-07-15 09:27:13 -0500

    Saving changes to reflect changes in functions.rb and views.rb song route.

--- app/models/functions.rb
+++ app/models/functions.rb
@@ -18,75 +18,35 @@ class Song
 end
 
 
-# Function adds the string being passed through the songfile variable into the 
-def songAdd2File(songfile)
-  File.open('music_db.txt', 'a') {|z| z.puts "#{songfile}" }
-end
 
-
-
-# If file exists, do nothing. Otherwise, create the music_db file with the first row being headers.
-def createFileWithHeader 
-  # TODO - See about using !File.exist?("music_db.txt")
-  if File.exist?("music_db.txt")
-  else
-  open('music_db.txt', 'w') { |z|
-    z << "Title||Artist||Album||Genre||Minutes||Seconds||Song Rating||Album Rating\n"
-  }
-  end 
-end
-
-
-# Create an array of each line in the file as a variable.
-def createFileArray
-  if File.exist?("music_db.txt")
-    y = File.foreach('music_db.txt').map { |line| line.split("||") }
-    z = y.uniq! #De-duping the output from the file. 
+  # Function adds the string being passed through the songfile variable into the 
+  def songAdd2File(songfile)
+    File.open('music_db.txt', 'a') {|z| z.puts "#{songfile}" }
   end
-  return z 
-end
-
-# Assign variable to hold array created from file.
-@x = createFileArray()
-
-if !@x.nil?
-  # Create variable that has the music_db file 
-  @x_no_h = @x.drop(1)
-
-  # Create groupings by each array element. 
-  @grouped = @x_no_h.group_by{|z| z[0]}.values
-end
-
-# TODO ---------------- Everything below this line should be
-#                       handled in the view.
-
-# Explicit html string for table header
-@tbl_header = 
-  "<thead>
-    <tr>
-    <th>Title</th>   <th>Artist</th>    <th>Album</th>        <th>Genre</th>
-    <th>Minutes</th>  <th>Seconds</th>  <th>Song Rating</th>  <th>Album Rating</th> 
-    </tr>
-  </thead>
-  <tbody>"
 
 
 
-def createHtmlTbl(grouped, tbl_header)
+  # If file exists, do nothing. Otherwise, create the music_db file with the first row being headers.
+  def createFileWithHeader 
+    # TODO - See about using !File.exist?("music_db.txt")
+    if File.exist?("music_db.txt")
+    else
+    open('music_db.txt', 'w') { |z|
+      z << "Title||Artist||Album||Genre||Minutes||Seconds||Song Rating||Album Rating\n"
+    }
+    end 
+  end
 
-  table = grouped.map do |portion|
 
-    "<table>\n" << tbl_header << "\n<tr>" << portion.map do |column|
 
-      "<td>" << column.map do |element|
+  # Create an array of each line in the file as a variable.
+  def createFileArray
+      y = File.foreach("music_db.txt").map { |line| line.split("||") }
+      # y_no_h = y.drop(1)
+      grouped = y.group_by{|x| x[0]}.values
+      return grouped 
+    # end
+  end
 
-        element.to_s
 
-      end.join("</td><td>") << "</td>"
 
-    end.join("</tr>\n<tr>") << "</tr>\n</tbody>\n</table>\n"
-
-  end.join("\n")
-
-  return table
-end
