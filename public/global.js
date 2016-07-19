b8f1b1307e5a1a499f792ffa4107a8b68bc998a0
John Morrow <CodeMorrow@yahoo.com>
2016-07-19 14:53:30 -0500

    modals

--- public/global.js
+++ public/global.js
@@ -4,9 +4,48 @@ window.addEventListener("load", function(){
 
   button.addEventListener("click", function(){
 
-  var modal = document.getElementById("similarArtist");
+  var modal = document.getElementById("errorMessage");
 
   modal.style.visibility = "visible";
+  });
+  
+});
+
+window.addEventListener("load", function(){
+
+  var button = document.getElementById("artistButton");
+
+  button.addEventListener("click", function(){
+
+  var modal = document.getElementById("similarArtists");
+
+  modal.style.visibility = "visible";
+  });
+  
+});
+
+window.addEventListener("load", function(){
+
+  var button = document.getElementById("addSongButton");
+
+  button.addEventListener("click", function(){
+
+  var addModal = document.getElementById("addSongForm");
+
+  addModal.style.visibility = "visible";
+  });
+  
+});
+
+window.addEventListener("load", function(){
+
+  var button = document.getElementById("deleteSongButton");
+
+  button.addEventListener("click", function(){
+
+  var deleteModal = document.getElementById("deleteSongForm");
+
+  deleteModal.style.visibility = "visible";
   });
   
 });
