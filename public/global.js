e6b898e0ebf23de07ba106d2b7f73268d39f1be6
John Morrow <CodeMorrow@yahoo.com>
2016-07-19 12:35:13 -0500

    create modal for Similar artist

---
+++ public/global.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+
+  var button = document.getElementById("artistButton");
+
+  button.addEventListener("click", function(){
+
+  var modal = document.getElementById("similarArtist");
+
+  modal.style.visibility = "visible";
+  });
+  
+});
