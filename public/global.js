5e2e808a6b4d6623bc518d9874159c6f9485b8d6
John Morrow <CodeMorrow@yahoo.com>
2016-07-20 20:49:47 -0500

    WIP - artist tabs

--- public/global.js
+++ public/global.js
@@ -1,60 +1,3 @@
-// ---------------------- Similar Artist Error Message Modal
-window.addEventListener("load", function(){
-
-  var button = document.getElementById("artistButton");
-
-  button.addEventListener("click", function(){
-
-  var modal = document.getElementById("errorMessage");
-
-  modal.style.visibility = "visible";
-  });
-  
-});
-
-// ---------------------- Close Similar Artist Modal JS
-window.addEventListener("load", function(){
-
-  var button = document.getElementById("artistClose");
-
-  button.addEventListener("click", function(){
-
-  var deleteModal = document.getElementById("errorMessage");
-
-  deleteModal.style.visibility = "hidden";
-  });
-  
-});
-
-
-// ---------------------- Similar Artist Modal JS
-window.addEventListener("load", function(){
-
-  var button = document.getElementById("artistButton");
-
-  button.addEventListener("click", function(){
-
-  var modal = document.getElementById("similarArtists");
-
-  modal.style.visibility = "visible";
-  });
-  
-});
-
-// ---------------------- Close Similar Artist Modal JS
-window.addEventListener("load", function(){
-
-  var button = document.getElementById("artistClose");
-
-  button.addEventListener("click", function(){
-
-  var deleteModal = document.getElementById("similarArtists");
-
-  deleteModal.style.visibility = "hidden";
-  });
-  
-});
-
 // ----------------------- Add Song Form Modal
 window.addEventListener("load", function(){
 
@@ -111,3 +54,29 @@ window.addEventListener("load", function(){
   });
   
 });
+
+//------------------------- Artist Tab JS
+
+function openAlbumTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("artistTab__link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
