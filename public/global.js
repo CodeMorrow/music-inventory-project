054cfdf0848a31413a308994beb08c4d1200d2c0
John Morrow <CodeMorrow@yahoo.com>
2016-07-22 10:20:51 -0500

    removed bad JS tab function

--- public/global.js
+++ public/global.js
@@ -57,28 +57,6 @@ window.addEventListener("load", function(){
 
 //------------------------- Artist Tab JS
 
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabContent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("artistTab__link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "flex";
-    evt.currentTarget.className += " active";
-}
-
-
 window.addEventListener("load", function(){
 
 var albumstab = document.getElementById("artistTab__albums");
